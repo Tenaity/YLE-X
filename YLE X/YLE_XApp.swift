46c65ff0867f66b32e9ad55bbaf6d246cc52bc70
Tenaity <tenaiti.150320@gmail.com>
2025-10-23 00:15:21 +0700

    Initial Commit

---
+++ YLE X/YLE_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  YLE_XApp.swift
+//  YLE X
+//
+//  Created by Tenaity on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct YLE_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YLE XTests/YLE_XTests.swift
@@ -0,0 +1,17 @@
+//
+//  YLE_XTests.swift
+//  YLE XTests
+//
+//  Created by Tenaity on 23/10/25.
+//
+
+import Testing
+@testable import YLE_X
+
+struct YLE_XTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
